e2aef990af9f18cad40fa8886a356380ec70c35e
SabinoWS <sabino.th@hotmail.com>
2024-04-30 18:52:18 -0300

    Adiciona instruções de exercícios

--- cliente.js
+++ cliente.js
@@ -2,27 +2,54 @@ import "./scss/cliente.scss"
 
 console.log("Iniciando contexto global: Página de cliente...")
 
+// Exercício 2
+// 0 - Ler todo o código e entender o que falta
+// 1 - Adicionar pontos faltantes, como click do botão de filtrar chamando iniciarFiltrarLivros
+// 2 - Construir função apiLivros_BuscarTodosOsLivros
+// 3 - Construir função tela_AdicionarLivrosNaTabela
+// 4 - Construir função utilitariosVetores_filtrarVetor
+// 5 - Testar unitaria mente função utilitariosVetores_filtrarVetor
+// 6 - Construir função iniciarFiltrarLivros para click do botão buscar
+// 7 - Separar funções em seus módulos (remova o que vem antes do underline _ e coloque em um arquivo com o mesmo nome)
+
 let livros = [] // Usar como memória de livros que foram buscados do servidor (API)
 // Adicionar callback function para keyup de botão de busca
 
-iniciar()
+iniciar() // Contexto global executando função iniciar ao entrar na tela
 
 async function iniciar() {
-    livros = await buscarTodosOsLivros()
-    adicionarLivrosNaTabela(livros)
+    livros = await apiLivros_BuscarTodosOsLivros()
+    tela_AdicionarLivrosNaTabela(livros)
 }
 
-async function buscarTodosOsLivros() {
+async function apiLivros_BuscarTodosOsLivros() {
     // return livros
 }
 
-function adicionarLivrosNaTabela(livros) {
+function tela_AdicionarLivrosNaTabela(livros) {
     // Construir tbody com os livros
 }
 
-async function filtrarLivros() {
-    // Filtrar da variável livros
+function iniciarFiltrarLivros() {
+    // Buscar valor da tela
+    // filtrarLivros(livros, valorDeBusca)
     // adicionarLivrosNaTabela(livrosFiltrados)
+}
+
+/* Testar função abaixo exemplo (
+    const listaOriginal = [
+        { nome: "João", idade: 20 },
+        { nome: "Maria", idade: 30 },
+        { nome: "José", idade: 25 },
+        { nome: "João", idade: 12 },
+    ]
+    const valorDeBusca = "João"
+    const propriedade = "nome"
+    const resultado = filtrarVetor(listaOriginal, valorDeBusca, propriedade)
+    console.log(resultado)
+    // Deve retornar [{ nome: "João", idade: 20 }, { nome: "João", idade: 12 }]
+) */
+function utilitariosVetores_filtrarVetor(listaOriginal, valorDeBusca, propriedade) {
 }
 
 
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import "./scss/index.scss"
 
 console.log("Iniciando contexto global: Página de usuário...")
+
+// Exercício 1
+// 1 - Adicionar evento de click no botão de execução
+// 2 - Criar função de início do processo
+// 3 - Criar função para envio de dados para a API
+// 4 - Informar tela sobre sucesso de cadastro
